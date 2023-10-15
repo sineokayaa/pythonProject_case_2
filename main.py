ff42717f8775cd3d9f4f1a67a8b6695479a528c5
sineokayaa <a.sineokaya@g.nsu.ru>
2023-10-15 17:02:58 +0700

    04

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ text = r.text
 
 names = []
 codes = []
+brands = []
+discounts = []
+prices = []
 
 ind_1 = text.find('найдено') + 8
 ind_2 = text.find('результатов')
@@ -24,33 +27,53 @@ for i in range(pages):
     url = 'https://www.lamoda.ru/catalogsearch/result/?q=' + req + '&pages=' + str(i + 1)
     r = requests.get(url)
     text = r.text
-    #print(text)
+    # print(text)
 
     ind_1 = text.find('<span class="d-multifilters-skeleton__checkbox">')
     ind_2 = text.find('data-form-action-login="/customer/account/login/"')
     text = text[ind_1:ind_2]
-    print(text)
 
     for j in range(pag):
         start = text.find('<div class="x-product-card__card">')
-        block = text[start + 56:]
+        block = text[start + 34:]
         end = block.find('<div class="x-product-card__card">')
         block = block[:end]
         link_1 = block.find('<a href=') + 9
-        link_2 = block.find('class="x-product-card__link') - 2
-        link = 'https://www.lamoda.ru' + block[link_1:link_2]
-        name_1 = block.find('<div class="x-product-card-description__product-name">') + 55
+        link = block[link_1:]
+        link_2 = link.find('class="x-product-card__link') - 2
+        link = 'https://www.lamoda.ru/' + link[:link_2]
+        '''name_1 = block.find('<div class="x-product-card-description__product-name">') + 55
         name_2 = block.find('</div></div></div></a>')
         name = block[name_1:name_2]
-        names.append(name)
+        names.append(name)'''
         text = text[start:]
         text = text[end:]
-        #print(block, j)
-        #print(link, j)
 
-        #item = requests.get(link)
-        #item = item.text
+        item = requests.get(link)
+        item = item.text
+        info_1 = item.find('user: {')
+        info_2 = item.find('request: {')
+        info = item[info_1:info_2]
+        brand_1 = info.find('"seo_tail":"brand-') + 18
+        brand = info[brand_1:]
+        brand_2 = brand.find('"title":') - 2
+        brand = brand[:brand_2]
+        brands.append(brand)
+        price_1 = item.find(',"price":') + 9
+        price = item[price_1:]
+        price_2 = price.find(',')
+        price = price[:price_2]
+        prices.append(price)
+        if '"type":"discount"' in info:
+            discount_1 = info.find(',"badges":[{"text":"') + 20
+            discount = info[discount_1:]
+            discount_2 = discount.find('","type":"discount"')
+            discount = discount[:discount_2]
+            discounts.append(discount)
 
+        else:
+            discounts.append('0')
+print(prices)
 
 '''
 ind_1 = text.find('<span class="d-multifilters-skeleton__checkbox">')
