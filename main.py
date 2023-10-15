e44fe75625947c8baade1421b8fd2b4e97ed1982
Varfolomeevaa <v.varfolomeeva@g.nsu.ru>
2023-10-15 22:02:15 +0700

    итаовидтвао

--- main.py
+++ main.py
@@ -34,32 +34,47 @@ for i in range(pages):
         block = text[start + 34:]
         end = block.find('<div class="x-product-card__card">')
         block = block[:end]
+
         link_1 = block.find('<a href=') + 9
         link = block[link_1:]
         link_2 = link.find('class="x-product-card__link') - 2
+
+
         code = link[3:link_2]
         end_code = code.find('/')
         code = code[:end_code].upper()
+
         link = 'https://www.lamoda.ru/' + link[:link_2]
+
+        #print(link)
+        #print(code)
 
         item = requests.get(link)
         item = item.text
+
         info_1 = item.find('user: {')
         info_2 = item.find('request: {')
         info = item[info_1:info_2]
+
         brand_1 = info.find('"seo_tail":"brand-') + 18
         brand = info[brand_1:]
         brand_2 = brand.find('"title":') - 2
         brand = brand[:brand_2]
 
+        #print(brand)
+
         name_1 = block.find('<div class="x-product-card-description__product-name">') + 55
         name_2 = block.find('</div></div></div></a>')
         name = block[name_1:name_2]
+
+        #print(name)
 
         price_1 = item.find(',"price":') + 9
         price = item[price_1:]
         price_2 = price.find(',')
         price = price[:price_2]
+
+        #print(price)
 
         if '"type":"discount"' in info:
             discount_1 = info.find(',"badges":[{"text":"') + 20
@@ -70,16 +85,21 @@ for i in range(pages):
         else:
             discount = '0'
 
+        #print(discount)
         country_1 = item.find('":"Страна производства","value":"') + 33
         country = item[country_1:]
         country_2 = country.find('"}')
         country = country[:country_2]
 
+        #print(country)
+
         text = text[start:]
         text = text[end:]
-        table[price] = [code, name, brand, country, discount]
 
-table = dict(sorted(table.items(), reverse=True))
+        table[code] = [price, name, brand, country, discount]
+        #print(table)
+
+
 with open('table.txt', 'w') as f:
     print('Артикул', 'Наименование', 'Бренд', 'Страна производства', 'Скидка', 'Цена', file=f)
     for key, value in table.items():
