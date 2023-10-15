92e2f133e167914baaa3f9a06fa56565f4c51c45
Varfolomeevaa <v.varfolomeeva@g.nsu.ru>
2023-10-15 11:21:07 +0700

    адааиав

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ req = input('Введите ваш запрос ')
 url = 'https://www.lamoda.ru/catalogsearch/result/?q='+req
 r = requests.get(url)
 text = r.text
-#print(text)
 
+ind_1 = text.find('найдено') + 8
+ind_2 = text.find('результатов')
+res = int(text[ind_1:ind_2])
+if res % 60 == 0:
+    pages = res // 60
+else:
+    pages = res // 60 + 1
 
+for i in range(pages):
+    url = 'https://www.lamoda.ru/catalogsearch/result/?q=' + req + '&pages=' + str(i + 1)
+    print(url)
 #ind = text.find('<span class="d-multifilters-skeleton__checkbox">')
 #text = text[ind:]
-print(text)
+#print(text)
