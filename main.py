ab83a80fee34da48feeb8002b85298bda534f744
sineokayaa <a.sineokaya@g.nsu.ru>
2023-10-15 12:47:31 +0700

    03

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import requests
+
 req = 'куртка женская'
-url = 'https://www.lamoda.ru/catalogsearch/result/?q='+req
+url = 'https://www.lamoda.ru/catalogsearch/result/?q=' + req
 r = requests.get(url)
 text = r.text
+
+names = []
+codes = []
 
 ind_1 = text.find('найдено') + 8
 ind_2 = text.find('результатов')
@@ -27,20 +31,30 @@ for i in range(pages):
         link_1 = text.find('<a href=') + 9
         link_2 = text.find('class="x-product-card__link') - 2
         link = 'https://www.lamoda.ru' + text[link_1:link_2]
+        code = text[link_1+3:link_1+15].upper()
+        codes.append(code)
         name_1 = text.find('<div class="x-product-card-description__product-name">') + 55
         name_2 = text.find('</div></div></div></a>')
         name = text[name_1:name_2]
+        names.append(name)
+        item = requests.get(link)
+        item = item.text()
 '''
-
 ind_1 = text.find('<span class="d-multifilters-skeleton__checkbox">')
 ind_2 = text.find('data-form-action-login="/customer/account/login/"')
 text = text[ind_1:ind_2]
-#print(text)
 start = text.find('<div class="x-product-card-description__microdata-wrap">')
 text=text[start + 56:]
 end = text.find('<img')
 text=text[:end]
+link_1 = text.find('<a href=') + 9
+link_2 = text.find('class="x-product-card__link') - 2
+link = 'https://www.lamoda.ru' + text[link_1:link_2]
+item = requests.get(link)
+item = item.text
+print(item)
+'''
 name_1 = text.find('<div class="x-product-card-description__product-name">') + 54
 name_2 = text.find('</div></div></div></a>')
-name = text[name_1+1:name_2]
-print(name)
+name = text[name_1 + 1:name_2]
+print(name)'''
