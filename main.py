df068618b5ff912547c6940c08ce38bcd631d7b0
sineokayaa <a.sineokaya@g.nsu.ru>
2023-10-16 20:30:29 +0700

    final_1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 Title
 Group:
-Sineokaya Anastasia 58
+Sineokaya Anastasia 60
 Varfolomeeva Viktoria 55
 """
 import requests
@@ -109,8 +109,6 @@ table = pd.DataFrame(
     {'Code': codes, 'Name': names, 'Price': prices, 'Discount': discounts, 'Brand': brands, 'Country': countries})
 pd.set_option('display.max_columns', 7)
 pd.set_option('display.max_rows', len(codes))
-final_table = table.sort_values(by=['Price'])
-print(final_table)
 
 with open('table.txt', 'w') as f:
     print(table, file=f)
