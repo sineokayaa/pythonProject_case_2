7c1f41fac20a738eaf8a6d36d62750ec28291bc1
sineokayaa <a.sineokaya@g.nsu.ru>
2023-10-16 17:39:29 +0700

    08

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import requests
+import pandas as pd
+import RU_LOCAL as RU
 
-req = input('Введите ваш запрос: ')
+req = input(RU.REQUEST)
 url = 'https://www.lamoda.ru/catalogsearch/result/?q=' + req
 r = requests.get(url)
 text = r.text
 
-table = {}
+codes = []
+prices = []
+names = []
+brands = []
+discounts = []
+countries = []
 
-ind_1 = text.find('найдено') + 8
-ind_2 = text.find('результатов')
+ind_1 = text.find(RU.FOUND) + 8
+ind_2 = text.find(RU.RESULTS)
 res = int(text[ind_1:ind_2])
 if res % 60 == 0:
     pages = res // 60
 else:
     pages = res // 60 + 1
-
-if res < 60:
-    pag = res
-else:
-    pag = 60
 
 
 for i in range(pages):
@@ -53,6 +55,7 @@ for i in range(pages):
         code = link[3:link_2]
         end_code = code.find('/')
         code = code[:end_code].upper()
+        codes.append(code)
 
         link = 'https://www.lamoda.ru/' + link[:link_2]
 
@@ -72,12 +75,14 @@ for i in range(pages):
         brand = info[brand_1:]
         brand_2 = brand.find('"title":') - 2
         brand = brand[:brand_2]
+        brands.append(brand)
 
         #print(brand)
 
         name_1 = block.find('<div class="x-product-card-description__product-name">') + 55
         name_2 = block.find('</div></div></div></a>')
         name = block[name_1:name_2]
+        names.append(name)
 
         #print(name)
 
@@ -85,6 +90,7 @@ for i in range(pages):
         price = item[price_1:]
         price_2 = price.find(',')
         price = price[:price_2]
+        prices.append(price)
 
         #print(price)
 
@@ -93,27 +99,40 @@ for i in range(pages):
             discount = info[discount_1:]
             discount_2 = discount.find('","type":"discount"')
             discount = discount[:discount_2]
+            discounts.append(discount)
 
         else:
             discount = '0'
+            discounts.append(discount)
 
         #print(discount)
-        country_1 = item.find('":"Страна производства","value":"') + 33
+        country_1 = item.find(RU.COUNTRIES) + 33
         country = item[country_1:]
         country_2 = country.find('"}')
         country = country[:country_2]
+        countries.append(country)
 
         #print(country)
 
         text = text[start:]
         text = text[end:]
 
-        table[code] = [name, brand, country, price, discount]
+        #table[code] = [name, brand, country, price, discount]
 
+print(countries)
+print(names)
+print(codes)
+print(discounts)
+print(brands)
+print(prices)
 
-#print(table)
+table = pd.DataFrame({RU.CODE:codes, RU.NAME:names, RU.PRICE:prices, RU.DISCOUNT:discounts, RU.BRAND:brands, RU.COUNTRY:countries})
+pd.set_option('display.max_columns', 7)
+pd.set_option('display.max_rows', len(codes))
+
+final_table = table.sort_values(by=[RU.PRICE])
+
+print(final_table)
 
 with open('table.txt', 'w') as f:
-    print('Артикул', 'Наименование', 'Бренд', 'Страна производства', 'Цена', 'Скидка', file=f)
-    for key, value in table.items():
-        print(key, *value,  file=f)
+    print(final_table, file=f)
